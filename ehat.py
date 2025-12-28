eb8200769485a8efbba4c8af7fee80b6a0341c95
batoolahmad947-prog <batoolahmad947@gmail.com>
2025-12-28 20:38:50 +0300

    Create ehat.py

---
+++ ehat.py
@@ -0,0 +1,18 @@
+def show_banner():
+    print("\033[91m")
+    print(r"""
+███████╗██╗  ██╗ █████╗ ████████╗
+██╔════╝██║  ██║██╔══██╗╚══██╔══╝
+█████╗  ███████║███████║   ██║
+██╔══╝  ██╔══██║██╔══██║   ██║
+███████╗██║  ██║██║  ██║   ██║
+╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝
+
+EHAT - Email Header Analysis Tool
+Phishing Detection & Forensic Analysis
+
+""")
+    print("\033[0m")
+
+def main():
+    print("[*] EHAT is running...")
